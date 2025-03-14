4d9232860ebd7ce3bebbe80cee9a1cc47b5a36a9
Vuong Dac Bac <146964299+dacbac2702@users.noreply.github.com>
2025-03-14 15:42:28 +0700

    day 2: set up password jwt

--- backend/server.js
+++ backend/server.js
@@ -2,7 +2,10 @@ require("dotenv").config();
 const express = require("express");
 const cors = require("cors");
 const mongoose = require("mongoose");
-
+const JwtStrategy = require("passport-jwt").Strategy,
+  ExtractJwt = require("passport-jwt").ExtractJwt;
+const passport = require("passport");
+const User = require("./models/User");
 const app = express();
 app.use(cors());
 app.use(express.json());
@@ -18,6 +21,27 @@ mongoose
 app.get("/", (req, res) => {
   res.send("Music Streaming API is running...");
 });
+
+// Cài đặt passport-jwt
+let opts = {};
+opts.jwtFromRequest = ExtractJwt.fromAuthHeaderAsBearerToken();
+opts.secretOrKey = "thisKeyIsSupposedToBeSecret";
+passport.use(
+  new JwtStrategy(opts, function (jwt_payload, done) {
+    User.findOne({ _id: jwt_payload.identifier }, function (err, user) {
+      // done(error, doesTheUserExist)
+      if (err) {
+        return done(err, false);
+      }
+      if (user) {
+        return done(null, user);
+      } else {
+        return done(null, false);
+        // Trả về không có, phải tạo tài khoản
+      }
+    });
+  })
+);
 
 const PORT = process.env.PORT || 5000;
 app.listen(PORT, () => console.log(`Server running on port ${PORT}`));
