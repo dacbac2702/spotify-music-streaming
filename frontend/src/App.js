5453808996a59741089a203d56b5a76f03752b3f
Vuong Dac Bac <146964299+dacbac2702@users.noreply.github.com>
2025-03-26 03:20:19 +0700

    add tailwind css to reactjs

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,12 +1,10 @@
 import React from "react";
-import MusicPlayer from "./components/MusicPlayer";
 
 function App() {
   return (
-    <div className="flex flex-col items-center justify-center min-h-screen bg-gray-900 text-white">
-      <h1 className="text-3xl font-bold mb-4">🎵 My Music App</h1>
-      <MusicPlayer songId="67dc54b0bfce6eb73d2aa038" />
-    </div>
+    <h1 className="text-3xl font-bold underline">
+      Hello world!
+    </h1>
   );
 }
 
